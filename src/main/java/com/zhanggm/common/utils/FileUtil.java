8216233e36ffb7837ad61ea21fa9b2e2f87e63b7
zhangguoming <zhanggm1002@qq.com>
2020-01-02 14:38:33 +0800

    字符串和文件工具类，添加方法

--- src/main/java/com/zhanggm/common/utils/FileUtil.java
+++ src/main/java/com/zhanggm/common/utils/FileUtil.java
@@ -1,5 +1,11 @@
 package com.zhanggm.common.utils;
 
 public class FileUtil {
-
+	
+	public String getExtName(String str) {
+		if(StringUtil.isNull(str)) {
+			return null;
+		}
+		return str.substring(str.indexOf("."));
+	}
 }
--- src/main/java/com/zhanggm/common/utils/StringUtil.java
+++ src/main/java/com/zhanggm/common/utils/StringUtil.java
@@ -1,5 +1,16 @@
 package com.zhanggm.common.utils;
 
 public class StringUtil {
+	
+	public static boolean isNull(String str) {
+		if(str==null || "".equals(str)) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean isNotNull(String str) {
+		return !isNull(str);
+	}
 
 }
